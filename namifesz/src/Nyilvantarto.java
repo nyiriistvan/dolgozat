70c2552e32d1223aeeacbccf0ebb99b26ae355ff
nyiriistvan <nyiri.istvan19@gmail.com>
2022-10-24 15:58:06 +0200

    Kész a Nyilvantarto

--- namifesz/src/Nyilvantarto.java
+++ namifesz/src/Nyilvantarto.java
@@ -1,3 +1,15 @@
-public class Nyilvantarto {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Nyilvantarto implements Nev {
+
+    @Override
+    public ArrayList<String> getNames() {
+        
+        String[] nevek = {"Béla", "Géza"};
+        ArrayList<String> nevLista=
+        new ArrayList<>(Arrays.asList(nevek));
+        return nevLista;
+    }
     
 }
