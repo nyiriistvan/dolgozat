7b58a5419661e69cd5ec9eea72dea91b7effd441
nyiriistvan <nyiri.istvan19@gmail.com>
2022-10-24 15:58:18 +0200

    Kész a feladat

--- namifesz/src/App.java
+++ namifesz/src/App.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Nyiri István,Szoft II N,22-10-24");
+        Nyilvantarto nyilvantarto=new Nyilvantarto();
+        ArrayList<String> nevlista=nyilvantarto.getNames();
+        for (String nev: nevlista){
+        System.out.println(nev);
+        }
+
     }
 }
